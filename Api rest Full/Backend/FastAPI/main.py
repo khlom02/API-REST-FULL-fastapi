1d6bbaf4a6d623a3995da455d4df9cbe763bc208
khlom02 <humanzombiemp@gmail.com>
2025-07-05 13:40:42 -0400

    Primer commit

---
+++ Api rest Full/Backend/FastAPI/main.py
@@ -0,0 +1,44 @@
+# Documentación oficial: https://fastapi.tiangolo.com/es/
+
+# Instala FastAPI: pip install "fastapi[all]"
+
+from fastapi import FastAPI
+from routers import products, users, basic_auth_users, jwt_auth_users, users_db
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+
+app.include_router(products.router)
+app.include_router(users.router)
+
+
+app.include_router(basic_auth_users.router)
+
+
+app.include_router(jwt_auth_users.router)
+
+
+app.include_router(users_db.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+
+
+@app.get("/")
+async def root():
+    return "Hola FastAPI!"
+
+
+
+@app.get("/url")
+async def url():
+    return {"url": "https://mouredev.com/python"}
+
+# Inicia el server: uvicorn main:app --reload
+# Detener el server: CTRL+C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
